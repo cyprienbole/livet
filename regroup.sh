1d9b5ebd18797bc0d9f9c6b040ee7664e1afd091
Cyprien Bole <cyprien.bole@gmail.com>
2020-05-13 18:08:37 +0200

    initialisation du projet

---
+++ regroup.sh
@@ -0,0 +1,9 @@
+cd wetransfer-daafab/
+for i in *.TPS ; do
+    cat $i | grep -e , -e IMAGE | tr -d "\r" | tr "\n" ";"  > ../out/$i
+    echo $i >> ../out/$i
+    echo -e "\n" >> ../out/$i
+done
+
+cd ../out
+cat *.TPS | sed '/^$/d' | tr ' ' ';' > ../alltps.csv
